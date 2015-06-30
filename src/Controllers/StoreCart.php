9c7976490303db100d941ca7d0d8665d0afc550d
mj1618 <matthew.stephen.james@gmail.com>
2015-06-30 16:50:11 +0800

    store cart

--- src/Controllers/StoreCart.php
+++ src/Controllers/StoreCart.php
@@ -1,17 +1,28 @@
 <?php namespace App\AUI\Controllers;
 
+use App\AUI\Model\StoreProduct;
+use App\AUI\Model\StoreTypePrice;
+use Illuminate\Support\Facades\Redirect;
 use MJ1618\AdminUI\Controller\Controller;
 use Route;
 use Input;
-
+use Cart;
+use Session;
 class StoreCart extends Controller {
 
     function post(){
 
-        dd(Input::all());
+//        dd(Input::all());
 
+        $col = Cart::search(array('name' => Input::get('product'), 'options' => array('price'=>Input::get('price'),'select' => Input::get('select'))));
 
-//        Cart::add(uniqid())
+        if($col!==false){
+            Cart::update($col[0], Cart::get($col[0])["qty"]+Input::get('quantity'));
+        } else {
+            Cart::add(uniqid(), Input::get('product'), Input::get('quantity'), StoreTypePrice::find(Input::get('price'))->price/100.0, Input::all() );
+        }
+
+        return Redirect::to(Input::get('return'));
     }
 
     function routes(){
